c50daed99073a642e42f61b22cb57e36cd0cbb3e
Roman Pierson <roman@mdac-consulting.com>
2017-05-18 22:49:59 +0200

    Added Licence

--- src/main/java/com/mdac/vertx/web/accesslogger/configuration/pattern/RequestElement.java
+++ src/main/java/com/mdac/vertx/web/accesslogger/configuration/pattern/RequestElement.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2016-2017 Roman Pierson
+ * ------------------------------------------------------
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 
+ * which accompanies this distribution.
+ *
+ *     The Apache License v2.0 is available at
+ *     http://www.opensource.org/licenses/apache2.0.php
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
 package com.mdac.vertx.web.accesslogger.configuration.pattern;
 
 import java.util.Arrays;
